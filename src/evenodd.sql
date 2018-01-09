b16fd26268b40d253fb47b0694ea2ff0eb280d69
Ankit Pati <contact@ankitpati.in>
2018-01-09 23:38:56 +0530

    odd and even numbers

---
+++ src/evenodd.sql
@@ -0,0 +1,38 @@
+/* evenodd.sql */
+/* Date  : 09 January 2016
+ * Author: Ankit Pati
+ */
+
+use adbms;
+
+drop procedure if exists evenodd;
+delimiter $
+create procedure evenodd(in num integer unsigned)
+begin
+    if num % 2 = 0 then
+        select num as 'Even';
+    else
+        select num as 'Odd';
+    end if;
+end $
+delimiter ;
+
+call evenodd(43);
+call evenodd(50);
+/* end of evenodd.sql */
+
+/* OUTPUT
+
++------+
+| Odd  |
++------+
+|   43 |
++------+
+
++------+
+| Even |
++------+
+|   50 |
++------+
+
+*/
