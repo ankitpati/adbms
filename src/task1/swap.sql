0a49d6603c8dd803a482cd821efed69fd478bfda
Ankit Pati <contact@ankitpati.in>
2018-01-15 15:50:24 +0530

    ORA swap two integers

---
+++ src/task1/swap.sql
@@ -0,0 +1,30 @@
+/* swap.sql */
+/* Date  : 15 January 2016
+ * Author: Ankit Pati
+ */
+
+set serveroutput on;
+
+declare
+    num1 number default 50;
+    num2 number default 43;
+    tmp0 number;
+
+begin
+    dbms_output.put_line('Before Swap: ' || num1 || ' ' || num2);
+
+    tmp0 := num1;
+    num1 := num2;
+    num2 := tmp0;
+
+    dbms_output.put_line('After  Swap: ' || num1 || ' ' || num2);
+end;
+/
+/* end of swap.sql */
+
+/* OUTPUT
+
+Before Swap: 50 43
+After  Swap: 43 50
+
+*/
