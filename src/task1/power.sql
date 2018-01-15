109e3fd23a5148b086fed721380d9663dedf8012
Ankit Pati <contact@ankitpati.in>
2018-01-15 16:14:59 +0530

    ORA calculate power of a number

---
+++ src/task1/power.sql
@@ -0,0 +1,27 @@
+/* power.sql */
+/* Date  : 15 January 2016
+ * Author: Ankit Pati
+ */
+
+set serveroutput on;
+
+declare
+    base number;
+    powr number;
+    resl number;
+
+begin
+    base := 43;
+    powr :=  2;
+
+    resl := power(base, powr);
+    dbms_output.put_line('Result = ' || resl);
+end;
+/
+/* end of power.sql */
+
+/* OUTPUT
+
+Result = 1849
+
+*/
