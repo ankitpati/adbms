85bc9822c2003111137ef166d90ba25112cd8b1b
Ankit Pati <contact@ankitpati.in>
2018-01-23 01:03:18 +0530

    ORA first & last names of students by PRN

---
+++ src/task1/student.sql
@@ -0,0 +1,44 @@
+/* student.sql */
+/* Date  : 15 January 2016
+ * Author: Ankit Pati
+ */
+
+alter session set current_schema = adbms;
+set serveroutput on;
+
+drop table student;
+create table student (
+    prn   integer primary key,
+    fname char(100) not null,
+    lname char(100) not null
+);
+
+begin
+    <<getnames>>
+    declare
+        prn   integer default 506;
+        fname char(100);
+        lname char(100);
+    begin
+        insert into student values (506, 'Ankit', 'Pati');
+        insert into student values (543, 'Tiashaa', 'Chatterjee');
+
+        select fname, lname
+        into   fname, lname
+        from   student
+        where  prn = getnames.prn;
+
+        dbms_output.put_line('PRN  ' || prn);
+        dbms_output.new_line;
+        dbms_output.put_line('Name ' || fname || ' ' || lname);
+    end;
+end;
+/
+/* end of student.sql */
+
+/* OUTPUT
+
+PRN  506
+Name Ankit Pati
+
+*/
