df31890ff25e98e9a968a06b65de6b4f2ed72da1
Ankit Pati <contact@ankitpati.in>
2018-01-29 12:40:32 +0530

    check for primality

---
+++ src/task3/isprime.sql
@@ -0,0 +1,42 @@
+/* isprime.sql */
+/* Date  : 29 January 2016
+ * Author: Ankit Pati
+ */
+
+alter session set current_schema = adbms;
+set serveroutput on;
+
+create or replace function isprime(n in integer)
+    return boolean deterministic is
+begin
+    if n < 2 then
+        return false;
+    end if;
+
+    for i in 2..(n/2) loop
+        if mod(n, i) = 0 then
+            return false;
+        end if;
+    end loop;
+    return true;
+end;
+/
+
+declare
+    ispr boolean := isprime(43);
+begin
+    dbms_output.put_line(
+        case
+            when     ispr then 'Prime'
+            when not ispr then 'Composite'
+        end
+    );
+end;
+/
+/* end of isprime.sql */
+
+/* OUTPUT
+
+Prime
+
+*/
