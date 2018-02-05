e324f6e5fcf81334cff3e6c3783a7a3aef62f62f
Ankit Pati <contact@ankitpati.in>
2018-02-05 14:56:43 +0530

    count before and after deletion

--- src/task4/employee.sql
+++ src/task4/employee.sql
@@ -18,33 +18,33 @@ insert into employee values (435, 'Sahil Pandey', NULL);
 
 create or replace procedure remove_emp(p_empid in integer)
     is
+    pre_count integer;
+    pst_count integer;
+
 begin
+    select count(*)
+    into   pre_count
+    from   employee;
+
     delete
     from   employee
     where  empid = p_empid;
+
+    select count(*)
+    into   pst_count
+    from   employee;
+
+    dbms_output.put_line('Count before deletion: ' || pre_count);
+    dbms_output.put_line('Count after  deletion: ' || pst_count);
 end;
 /
 
-select * from employee;
 exec remove_emp(435);
-select * from employee;
 /* end of employee.sql */
 
 /* OUTPUT
 
-EMPID ENAME              SALRY
------ ------------------ -----
-  506 Ankit Pati         143
-  543 Tiashaa Chatterjee 543
-  435 Sahil Pandey
-
-
-PL/SQL procedure successfully completed.
-
-
-EMPID ENAME              SALRY
------ ------------------ -----
-  506 Ankit Pati         143
-  543 Tiashaa Chatterjee 543
+Count before deletion: 3
+Count after  deletion: 2
 
 */
