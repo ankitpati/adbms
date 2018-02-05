bc143af6eac3225558ee0554b058564b1eb758ae
Ankit Pati <contact@ankitpati.in>
2018-02-05 14:28:54 +0530

    compute square of a number

---
+++ src/task4/square.sql
@@ -0,0 +1,23 @@
+/* square.sql */
+/* Date  : 05 February 2016
+ * Author: Ankit Pati
+ */
+
+alter session set current_schema = adbms;
+set serveroutput on;
+
+create or replace procedure square(base in integer)
+    is
+begin
+    dbms_output.put_line(base || ' squared equals ' || base*base);
+end;
+/
+
+exec square(43);
+/* end of square.sql */
+
+/* OUTPUT
+
+43 squared equals 1849
+
+*/
