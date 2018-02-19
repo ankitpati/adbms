9d1563f721a30f17817c4ddf3fedb4a18656323e
Ankit Pati <contact@ankitpati.in>
2018-02-19 14:56:47 +0530

    cursor over experienced employees

---
+++ src/task6/experienced.sql
@@ -0,0 +1,61 @@
+/* experienced.sql */
+/* Date  : 19 February 2016
+ * Author: Ankit Pati
+ */
+
+alter session set current_schema = adbms;
+set serveroutput on;
+
+/* relying on Oracle built-in table emp */
+
+declare
+    cursor exp_cur is
+        select ename, job
+        from   emp
+        where  extract(year from sysdate) - extract(year from hiredate) > 36;
+begin
+    for exp_rec in exp_cur loop
+        dbms_output.put_line('Name : ' || exp_rec.ename);
+        dbms_output.put_line('Job  : ' || exp_rec.job);
+        dbms_output.put_line(chr(9));
+    end loop;
+end;
+/
+/* end of experienced.sql */
+
+/* OUTPUT
+
+Name : KING
+Job  : PRESIDENT
+
+Name : BLAKE
+Job  : MANAGER
+
+Name : CLARK
+Job  : MANAGER
+
+Name : JONES
+Job  : MANAGER
+
+Name : FORD
+Job  : ANALYST
+
+Name : SMITH
+Job  : CLERK
+
+Name : ALLEN
+Job  : SALESMAN
+
+Name : WARD
+Job  : SALESMAN
+
+Name : MARTIN
+Job  : SALESMAN
+
+Name : TURNER
+Job  : SALESMAN
+
+Name : JAMES
+Job  : CLERK
+
+*/
